82a59e58adc7fa9f9b1e045bf4ae059bdf57b469
Siman04 <122814329+Siman04@users.noreply.github.com>
2025-10-18 18:43:13 +0000

    ug

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,5 +1,4 @@
-IF NOT EXISTS 
-CREATE DATABASE alx_book_store;
+CREATE DATABASE IF NOT EXISTS  alx_book_store;
 USE alx_book_store;
 CREATE TABLE Authors (author_id INT PRIMARY KEY AUTO_INCREMENT,
 author_name VARCHAR (215) NOT NULL);
